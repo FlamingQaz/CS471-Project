d605951ea8384b696d486b5cb677c2e73f239f60
Ashe Visavale <flamingqaz@gmail.com>
2024-08-22 14:39:05 -0400

    Frees Path node when enemy reaches end

--- Assets/Prefab Objects/Enemies/Enemy_test_1.gd
+++ Assets/Prefab Objects/Enemies/Enemy_test_1.gd
@@ -8,5 +8,5 @@ func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	
 	if get_parent().get_progress_ratio() == 1:
-		queue_free()
+		get_parent().get_parent().queue_free()
  
