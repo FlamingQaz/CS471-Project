6cee68acf55663f417a158e3cd5d666f35989e56
ThePigWiggle <idmenold@gmail.com>
2024-09-16 14:09:09 -0400

    The Most Game Menu

--- Assets/Scripts/UI/Main Menu/MainMenu.gd
+++ Assets/Scripts/UI/Main Menu/MainMenu.gd
@@ -12,10 +12,10 @@ func _process(delta):
 
 
 func _on_play_button_pressed():
-	
+	get_tree().change_scene_to_file("res://Assets/Levels/Test_map.tscn");
 	pass # Replace with function body.
 
 
 func _on_quit_button_pressed():
-	GetTree.Quit();
+	get_tree().quit();
 	pass # Replace with function body.
